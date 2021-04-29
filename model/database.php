cb1b40a1e9c7873ff5bc3074a95e38220ae3651e
mmahoney1006 <75647451+mmahoney1006@users.noreply.github.com>
2021-04-29 18:31:19 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
 // Set up the database connection
-$dsn = 'mysql:host=pnw-cep-db.c2xjkeyaldgu.us-east-2.rds.amazonaws.com;dbname=pnw_cep';
+$dsn = 'mysql:host=pnw-cep-db.c2xjkeyaldgu.us-east-2.rds.amazonaws.com;dbname=pnw_cep;port=3306';
 $username = 'masterAdmin';
 $password = 'McM3012516';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
